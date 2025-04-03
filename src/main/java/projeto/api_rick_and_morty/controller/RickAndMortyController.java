5d58e02360852ee257234ddf9ee86d4c6ea5e18b
Paola Moura <paolamoura.kj@hotmail.com>
2025-04-03 19:50:26 -0300

    fix: config endpoints GET para buscar personagem e episodios

---
+++ src/main/java/projeto/api_rick_and_morty/controller/RickAndMortyController.java
@@ -0,0 +1,40 @@
+package projeto.api_rick_and_morty.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import projeto.api_rick_and_morty.client.RickAndMortyClient;
+import projeto.api_rick_and_morty.model.CharacterResponse;
+import projeto.api_rick_and_morty.model.ListOfEpisodesResponse;
+
+
+/**
+ * Controller responsável por expor endpoints para acessar informações da API pública do Rick and Morty.
+ *
+ * <p>Define as rotas da API e utiliza {@link RickAndMortyClient} para buscar dados da API externa.</p>
+ *
+ * <p>Os métodos estão mapeados com anotações do Spring, como {@GetMapping}.</p>
+ *
+ * @author @paolahegel
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/api/rick-and-morty")
+@AllArgsConstructor
+public class RickAndMortyController {
+
+    RickAndMortyClient rickAndMortyClient;
+
+    @GetMapping("/character/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CharacterResponse getCharacterById(@PathVariable String id) {
+        return rickAndMortyClient.findCharacterById(id);
+    }
+
+    @GetMapping("/episode")
+    @ResponseStatus(HttpStatus.OK)
+    public ListOfEpisodesResponse listAllEpisode() {
+        return rickAndMortyClient.listAllEpisodes();
+    }
+
+}
